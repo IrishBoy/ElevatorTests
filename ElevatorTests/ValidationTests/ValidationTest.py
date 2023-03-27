5dc753413d55dac232b59b84db465b8e65e1e44d
Maksimilian Mamilyaev <mmamilyaev@ozon.ru>
2023-03-27 22:57:23 +0200

    Added classes for Validation Tests

--- ElevatorTests/ValidationTests/ValidationTest.py
+++ ElevatorTests/ValidationTests/ValidationTest.py
@@ -1,7 +1,8 @@
 from .. import Test
 
 class VailidationTest(Test):
-     def __init__(self, author, info, service_names):
-          super().__init__(author, info)
-          self.service_names = service_names
-          # self.end_service = end_service
+     def __init__(self, author, info, elevator, services_list, input_parametrs, output_parameters):
+          super().__init__(author, info, elevator)
+          self.services_list = services_list
+          self.input_parametrs = input_parametrs
+          self.output_parameters = output_parameters
--- ElevatorTests/ValidationTests/ValidationTestUseCase1.py
+++ ElevatorTests/ValidationTests/ValidationTestUseCase1.py
@@ -1,4 +1,9 @@
 from ValidationTest import VailidationTest
 
 class ValidationTestUseCase1(VailidationTest):
-     pass
+     def __init__(self, author, info, elevator, services_list, input_parametrs, output_parameters):
+          super().__init__(author, info, elevator, services_list, input_parametrs, output_parameters)
+
+     
+     def run(self):
+          pass
