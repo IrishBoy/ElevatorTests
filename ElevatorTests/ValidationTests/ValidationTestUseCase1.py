2653e1726e9903029e6351d79a437f7dc48a6758
Maksimilian Mamilyaev <mmamilyaev@ozon.ru>
2023-03-28 10:24:10 +0200

    Validation test added

--- ElevatorTests/ValidationTests/ValidationTestUseCase1.py
+++ ElevatorTests/ValidationTests/ValidationTestUseCase1.py
@@ -6,4 +6,29 @@ class ValidationTestUseCase1(VailidationTest):
 
      
      def run(self):
-          pass
+          doors = self.doors()
+          moving = self.startMove(doors)
+          floorStop = self.stop(moving)
+          openDoor = self.openDoor(floorStop)
+          assert_result =  self.assertEqual([doors, moving, floorStop, openDoor], self.output_parameters)
+          return {
+             "result": assert_result,
+             "message": "" if assert_result == True else '''Validation test based on use case 1 failed'''
+          }
+
+
+     def openDoor(self, floorStop):
+          opedDoorPar = self.elevator.DoorOpened(floorStop, self.input_parametrs["DoorOpened"])
+          return opedDoorPar
+
+     def stop(self, moving):
+          stop = self.elevator.StopAtFloof(moving, self.input_parametrs["right_floor"])
+          return stop
+     
+     def startMove(self, between_doors):
+          moving = self.elevator.ElevatorMoving(between_doors,self.input_parametrs["selected_floor"])
+          return moving
+
+     def doors(self):
+          doors_result = self.elevator.Doors(self.input_parametrs["smth_between_doors"])
+          return doors_result
